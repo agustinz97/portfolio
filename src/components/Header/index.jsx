c381d7a6532619db9712e3439f53e3bc885a650d
Agustin Zamar <agustinzamar33@gmail.com>
2020-09-18 11:59:23 -0300

    agregada funcionalidad del header para que se cierre el menu al hacer click en el body

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 import { HeaderStyled } from './styles'
 
 export const Header = () => {
     const [active, setActive] = useState(false)
 
+    const menu = useRef(null)
+    const button = useRef(null)
+
     const toggleMenu = () => {
         setActive(prevActive => !prevActive)
     }
 
+    const closeOnBodyClick = () => {
+        document.addEventListener('click', evt => {
+            console.log(evt.target)
+
+            if (
+                !menu.current.contains(evt.target) &&
+                !button.current.contains(evt.target)
+            ) {
+                setActive(false)
+            }
+        })
+    }
+
+    useEffect(closeOnBodyClick, [])
+
     return (
         <HeaderStyled active={active}>
-            <nav>
+            <nav ref={menu}>
                 <ul>
                     <li>
                         <a href="/">Inicio</a>
@@ -27,7 +45,7 @@ export const Header = () => {
                     </li>
                 </ul>
             </nav>
-            <button className="header_btn" onClick={toggleMenu}>
+            <button className="header_btn" onClick={toggleMenu} ref={button}>
                 <span></span>
                 <span></span>
                 <span></span>
